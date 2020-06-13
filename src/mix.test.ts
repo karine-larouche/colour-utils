f7444429e24f26d5b8a2e15e57f7b85e2ed43708
Karine Larouche <karine.larouche14@gmail.com>
2020-06-13 11:18:31 -0400

    Add linter and formatter

--- src/mix.test.ts
+++ src/mix.test.ts
@@ -1,4 +1,4 @@
-import { mix } from '.'
+import { mix } from '.';
 
 test('The first colour is returned when the ratio is 0', () => {
   expect(mix('#123456', '#abcdef', 0)).toBe('#123456');
@@ -8,24 +8,24 @@ test('The second colour is returned when the ratio is 1', () => {
   expect(mix('#123456', '#abcdef', 1)).toBe('#abcdef');
 });
 
-test('The red quantity is the ponderated average of the 2 provided colour\'s red quantities', () => {
-  expect(mix('#120000', '#ee0000', .55)).toBe('#8b0000');
+test("The red quantity is the ponderated average of the 2 provided colour's red quantities", () => {
+  expect(mix('#120000', '#ee0000', 0.55)).toBe('#8b0000');
 });
 
-test('The green quantity is the ponderated average of the 2 provided colour\'s green quantities', () => {
-  expect(mix('#001200', '#00ee00', .55)).toBe('#008b00');
+test("The green quantity is the ponderated average of the 2 provided colour's green quantities", () => {
+  expect(mix('#001200', '#00ee00', 0.55)).toBe('#008b00');
 });
 
-test('The blue quantity is the ponderated average of the 2 provided colour\'s blue quantities', () => {
-  expect(mix('#000012', '#0000ee', .55)).toBe('#00008b');
+test("The blue quantity is the ponderated average of the 2 provided colour's blue quantities", () => {
+  expect(mix('#000012', '#0000ee', 0.55)).toBe('#00008b');
 });
 
 test('Unexact results are rounded', () => {
-  expect(mix('#000000', '#010203', .6)).toBe('#010102');
+  expect(mix('#000000', '#010203', 0.6)).toBe('#010102');
 });
 
 test('Upper and lower case letters are accepted', () => {
-  expect(mix('#000000', '#aaAA00', .5)).toBe('#555500');
+  expect(mix('#000000', '#aaAA00', 0.5)).toBe('#555500');
 });
 
 test('.5 ratio is used when no ratio is provided', () => {
@@ -34,13 +34,15 @@ test('.5 ratio is used when no ratio is provided', () => {
 
 describe('Error handling', () => {
   test('Providing an invalid first colour throws a meaningful error', () => {
-    const expected = 'First argument of "mix" is invalid. Expected hex colour string (e.g. #123456), but received ';
-    expect(() => mix('#000', '#000000', 0)).toThrowError(expected+'#000');
+    const expected =
+      'First argument of "mix" is invalid. Expected hex colour string (e.g. #123456), but received ';
+    expect(() => mix('#000', '#000000', 0)).toThrowError(expected + '#000');
   });
-  
+
   test('Providing an invalid second colour throws a meaningful error', () => {
-    const expected = 'Second argument of "mix" is invalid. Expected hex colour string (e.g. #123456), but received ';
-    expect(() => mix('#000000', '#000', 0)).toThrowError(expected+'#000');
+    const expected =
+      'Second argument of "mix" is invalid. Expected hex colour string (e.g. #123456), but received ';
+    expect(() => mix('#000000', '#000', 0)).toThrowError(expected + '#000');
   });
 
   test('Hex colour strings with fewer than 6 digits are not accepted', () => {
@@ -62,12 +64,16 @@ describe('Error handling', () => {
   });
 
   test('Providing a negative ratio throws a meaningful error', () => {
-    const expected = 'Third argument of "mix" is invalid. Expected number between 0 and 1 (inclusive), but received ';
-    expect(() => mix('#000000', '#000000', -0.1)).toThrowError(expected+'-0.1');
+    const expected =
+      'Third argument of "mix" is invalid. Expected number between 0 and 1 (inclusive), but received ';
+    expect(() => mix('#000000', '#000000', -0.1)).toThrowError(
+      expected + '-0.1',
+    );
   });
-  
+
   test('Providing a ratio greater than 1 throws a meaningful error', () => {
-    const expected = 'Third argument of "mix" is invalid. Expected number between 0 and 1 (inclusive), but received ';
-    expect(() => mix('#000000', '#000000', 1.1)).toThrowError(expected+'1.1');
+    const expected =
+      'Third argument of "mix" is invalid. Expected number between 0 and 1 (inclusive), but received ';
+    expect(() => mix('#000000', '#000000', 1.1)).toThrowError(expected + '1.1');
   });
 });
--- src/mix.ts
+++ src/mix.ts
@@ -1,12 +1,16 @@
 const numberToHexString = (amount: number) => {
   const str = amount.toString(16);
   return str.length === 2 ? str : `0${str}`;
-}
+};
 
-const fromHex = (hexColour: string) : number[] =>
-  [ hexColour.slice(1,3), hexColour.slice(3,5), hexColour.slice(5,7) ].map(c => parseInt(c, 16));
+const fromHex = (hexColour: string): number[] =>
+  [
+    hexColour.slice(1, 3),
+    hexColour.slice(3, 5),
+    hexColour.slice(5, 7),
+  ].map((c) => parseInt(c, 16));
 
-const toHex = (colour: number[]) : string =>
+const toHex = (colour: number[]): string =>
   colour.reduce((res, c) => res + numberToHexString(c), '#');
 
 /**
@@ -17,22 +21,30 @@ const toHex = (colour: number[]) : string =>
  * @param ratio The ratio of colourB in the resulting mix. Should be between `0` and `1`, inclusive. Default value is `0.5`.
  * @returns The colour resulting from mixing colourA and colourB, in rgb format (e.g. `'#000000'`).
  */
-export const mix = (colourA: string, colourB: string, ratio: number = .5) : string => {
+export const mix = (colourA: string, colourB: string, ratio = 0.5): string => {
   const hexRegex = /^#(\d|[a-f]|[A-F]){6}$/;
   if (!hexRegex.test(colourA)) {
-    throw new Error(`First argument of "mix" is invalid. Expected hex colour string (e.g. #123456), but received ${colourA}`);
+    throw new Error(
+      `First argument of "mix" is invalid. Expected hex colour string (e.g. #123456), but received ${colourA}`,
+    );
   }
   if (!hexRegex.test(colourB)) {
-    throw new Error(`Second argument of "mix" is invalid. Expected hex colour string (e.g. #123456), but received ${colourA}`);
+    throw new Error(
+      `Second argument of "mix" is invalid. Expected hex colour string (e.g. #123456), but received ${colourA}`,
+    );
   }
   if (ratio < 0 || ratio > 1) {
-    throw new Error(`Third argument of "mix" is invalid. Expected number between 0 and 1 (inclusive), but received ${ratio}`);
+    throw new Error(
+      `Third argument of "mix" is invalid. Expected number between 0 and 1 (inclusive), but received ${ratio}`,
+    );
   }
 
   const first = fromHex(colourA);
   const second = fromHex(colourB);
-  
-  const mixed = [0,1,2].map(i => Math.round((first[i] * (1 - ratio)) + (second[i] * ratio)));
+
+  const mixed = [0, 1, 2].map((i) =>
+    Math.round(first[i] * (1 - ratio) + second[i] * ratio),
+  );
 
   return toHex(mixed);
-}
+};
